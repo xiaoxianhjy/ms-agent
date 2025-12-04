9aadfa9a242b4679bcd3bf05dc53ec268192a0e2
Justin <74602715+xucailiang@users.noreply.github.com>
2025-12-04 14:31:44 +0800

    fix workflow example import error (#806)

--- examples/workflow/custom_code.py
+++ examples/workflow/custom_code.py
@@ -1,12 +1,16 @@
 from typing import List, Union
 
-from ms_agent.agent.code_agent import Code
+from ms_agent.agent.code_agent import CodeAgent
 from ms_agent.llm import Message
 
 
-class CustomCodeAgent(Code):
+class CustomCodeAgent(CodeAgent):
 
     async def run(self, inputs: Union[str, List[Message]],
-                  **kwargs) -> List[Message]:
+                **kwargs) -> List[Message]:
         print(f'Code executed in {self.tag}!')
+        if isinstance(inputs, str):
+            # This example doesn't handle string inputs, so convert to a list of one message
+            inputs = [Message(role='user', content=inputs)]
         inputs.append(Message(role='user', content='Calculate1+1'))
+        return inputs
